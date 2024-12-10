e98e763f9d90a41571d6a3c7185f8cfbf9c3a300
Adao Neto <adaonetohti@hotmail.com>
2024-12-10 17:41:38 -0300

    modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Tá esperando o que, meu nobre blablatente?</h1>
+    return <h1>Tá esperando o que, meu nobre combatente? 😏</h1>
     // Clica logo nesse <a href="https://www.ticketou.com/reveiover">LINK AQUI</a> e desenrola nosso rolê, por gentileza.
 
 }

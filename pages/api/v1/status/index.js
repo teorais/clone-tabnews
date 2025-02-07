ac8834f27d8673d7f33e153cc062d13d5c9c9794
Adao Neto <adaonetohti@hotmail.com>
2025-02-07 15:17:44 -0300

    push de teste

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
+import database from "../../../../infra/database.js";
+
 function status(request, response) {
+  console.log(database);
   response.status(200).json({ chave: "alunos do curso.dev são fodas" });
 }
 
